139170ce1790ca5ef1966f78a63b9c93b05d703d
vino4d <109666412+vino4d@users.noreply.github.com>
2022-12-14 01:01:34 +0530

    Add files via upload

---
+++ mean_var_std.py
@@ -0,0 +1,31 @@
+import numpy as np
+a=input()
+a=a.split(',')
+a=[int(i) for i in a]
+if len(a)!=9:
+    raise ValueError( "List must contain nine numbers.")
+a=np.array(a)
+a=a.reshape(3,3)
+
+mean=[]
+var=[]
+sd=[]
+min=[]
+max=[]
+sum=[]
+
+#calc fun to do np.func(f) with input val(v)
+def calculate(x,v,f):
+    x.append(list(f(v,axis=0)))
+    x.append(list(f(v,axis=1)))
+    x.append(f(v))
+
+calculate(mean,a,np.mean)
+calculate(var,a,np.var)
+calculate(sum,a,np.sum)
+calculate(sd,a,np.std)
+calculate(min,a,np.min)
+calculate(max,a,np.max)
+
+ans={'mean':mean,'variance':var,'standard deviation':sd,'min':min,'max':max,'sum':sum}
+print(ans)
